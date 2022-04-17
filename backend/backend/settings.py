c95c3204cfb00c798cdaf26790092376864223cd
xdjx3runx <xdjx3runx@gmail.com>
2022-04-17 15:50:22 -0700

    fixed all backend bugs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corseheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- backend/workoutJournal/admin.py
+++ backend/workoutJournal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Exercise, Workout, Count
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Exercise)
+admin.site.register(Workout)
+admin.site.register(Count)
