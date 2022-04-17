3a31eb4cd0a63502a809886e87ce1cca17b04207
xdjx3runx <xdjx3runx@gmail.com>
2022-04-16 17:36:39 -0700

    added custom user model

--- backend/workoutJournal/models.py
+++ backend/workoutJournal/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+
+
+
+
 
 # Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(default=timezone.now)
+    user_height = models.IntegerField(default=60)
+    user_weight = models.IntegerField(default=150)
+    image = models.ImageField(upload_to='images/')
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
