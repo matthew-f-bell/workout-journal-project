f51d0a9d6697da757a860db77f64a19073676fb0
PredatorR10 <godofgamingpr10@gmail.com>
2022-04-20 01:56:37 -0400

    added login component

---
+++ frontend/src/components/login/index.jsx
@@ -0,0 +1,18 @@
+const Login = () => {
+    return (
+        <>
+            <h2>Login</h2>
+            <form action="post">
+                <label for="email">E-mail</label>
+                <input type="text" name="email"/>
+
+                <label for="password">Password</label>
+                <input type="text" name="password"/>
+
+                <input type="submit" value="Login"/>
+            </form>
+        </>
+    )
+}
+
+export default Login;
