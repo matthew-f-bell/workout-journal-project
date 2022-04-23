fbdf144532a2a207afd1a8e14296f033fbf54227
PredatorR10 <godofgamingpr10@gmail.com>
2022-04-23 19:22:08 -0400

    added user and count serializer, view, and url

--- backend/workoutJournal/serializers.py
+++ backend/workoutJournal/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import User, Exercise, Workout
+from .models import Count, User, Exercise, Workout
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email')
+        fields = ('id', 'email', 'password','first_name', 'last_name', 'date_joined', 'user_height', 'user_weight', 'image', 'is_staff', 'is_superuser')
 
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,8 @@ class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
         fields = ('id', 'creator', 'name', 'exercises')
+
+class CountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Count
+        fields = ('id', 'workout', 'exercise', 'reps', 'sets')
