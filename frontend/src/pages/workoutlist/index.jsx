139ac2207d426ec5d282b2ba1a0264e58134c58e
PredatorR10 <godofgamingpr10@gmail.com>
2022-04-26 23:18:41 -0400

    Workout list will now render a workout component for each workout

--- frontend/src/pages/workoutlist/index.jsx
+++ frontend/src/pages/workoutlist/index.jsx
@@ -1,12 +1,34 @@
 import NavBar from "../../components/navbar";
 import Workout from "../../components/workout";
 
+import * as workoutService from "../../api/workout.service"
+import { useEffect, useState } from "react";
+
 const WorkoutList = () => {
+    const [workouts, setWorkouts] = useState([])
+
+    const getWorkouts = async () => {
+        await workoutService.getAll().then((res) => {
+            setWorkouts(res.data)
+        })
+    }
+
+    useEffect(() => {
+        getWorkouts()
+    }, [])
+
     return(
         <>
             <NavBar/>
             <h1>Workout List</h1>
-            <Workout/>
+            {console.log(workouts)}
+            {workouts.map((workout) => {
+                return (
+                    <>
+                        <Workout name={workout.name}/>
+                    </>
+                )
+            })}
         </>
     )
 }
