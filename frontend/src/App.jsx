ed863fb1674132fcd27d101fbfc6856e58ca7cb4
xdjx3runx <xdjx3runx@gmail.com>
2022-04-30 12:40:42 -0700

    created signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/homepage"
 import ProfilePage from './pages/profilepage';
+import SignupPage from './pages/signuppage';
 import WorkoutList from './pages/workoutlist';
 
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage/>}/>
           <Route path="/workouts" element={<WorkoutList/>}/>
           <Route path="/profile" element={<ProfilePage/>}/>
+          <Route path="/register" element={<SignupPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
